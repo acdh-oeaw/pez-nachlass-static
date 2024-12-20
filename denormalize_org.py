6b770d7f7194b42d355cbc9f8e2dce637d7779af
csae8092 <p.andorfer@gmail.com>
2024-12-20 08:12:53 +0100

    adds listorgs

--- denormalize_org.py
+++ denormalize_org.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import lxml.etree as ET
 from collections import defaultdict
 from acdh_tei_pyutils.tei import TeiReader
 from acdh_xml_pyutils.xml import NSMAP
@@ -20,7 +21,18 @@ for f in files:
     filename = os.path.basename(f)
     doc = TeiReader(f)
     root = doc.any_xpath(".//tei:profileDesc/tei:abstract")[0]
+    for x in root.xpath(".//tei:org", namespaces=NSMAP):
+        x.getparent().remove(x)
+    listorg = ET.Element("{http://www.tei-c.org/ns/1.0}listOrg")
+    root.insert(1, listorg)
     for org in d[filename]:
-        root.insert(org, 1)
+        listorg.append(org)
     doc.tree_to_file(f)
-    
+
+print("now lets delete all empty listPerson and listOrg elements")
+
+for f in files:
+    doc = TeiReader(f)
+    for empty_node in doc.any_xpath(".//tei:listPerson[not(node())] | .//tei:listOrg[not(node())]"):
+        empty_node.getparent().remove(empty_node)
+    doc.tree_to_file(f)
